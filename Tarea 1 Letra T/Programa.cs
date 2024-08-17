3c6e5e7b9797b5022ee6c93e4adeda424248e91d
EdmundoZ12 <edmundozentenor12@gmail.com>
2024-08-17 04:05:52 -0400

    Primer tarea

---
+++ Tarea 1 Letra T/Programa.cs
@@ -0,0 +1,14 @@
+﻿namespace Introduccion
+{
+    public class Programa
+    {
+        public static void Main(string[] args)
+        {
+            using (Ventana ventana = new Ventana())
+            {
+                ventana.Run();
+            }
+        }
+
+    }
+}
---
+++ Tarea 1 Letra T/Ventana.cs
@@ -0,0 +1,140 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using OpenTK.Mathematics;
+
+namespace Introduccion
+{
+    public class Ventana : GameWindow
+    {
+        private int vertexBufferHandle;
+        private int shaderProgramHandle;
+        private int vertexArrayHandle;
+
+        public Ventana() : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            this.CenterWindow(new Vector2i(1280, 768));
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, e.Width, e.Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnLoad()
+        {
+            GL.ClearColor(Color4.Black);
+
+            float[] vertices = new float[]
+            {
+                // Parte superior de la "T"
+                -0.5f,  0.5f, 0f,   // Vertice 0
+                 0.5f,  0.5f, 0f,   // Vertice 1
+                 0.5f,  0.3f, 0f,   // Vertice 2
+                -0.5f,  0.3f, 0f,   // Vertice 3
+
+                // Parte vertical de la "T"
+                -0.1f,  0.3f, 0f,   // Vertice 4
+                 0.1f,  0.3f, 0f,   // Vertice 5
+                 0.1f, -0.5f, 0f,   // Vertice 6
+                -0.1f, -0.5f, 0f    // Vertice 7
+            };
+
+            this.vertexBufferHandle = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, this.vertexBufferHandle);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+            this.vertexArrayHandle = GL.GenVertexArray();
+            GL.BindVertexArray(this.vertexArrayHandle);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, this.vertexBufferHandle);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            string vertexShaderCode =
+                @"
+                #version 330 core
+
+                layout (location =0 ) in vec3 aPosition;
+
+                void main()
+                {
+                    gl_Position = vec4(aPosition, 1f);
+                }
+                ";
+
+            string pixelShaderCode =
+                @"
+                #version 330 core
+
+                out vec4 pixelColor;
+
+                void main()
+                {
+                    pixelColor=vec4(0.8f, 0.8f, 0.1f, 1f);
+                }
+                ";
+
+            int vertexShaderHandle = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShaderHandle, vertexShaderCode);
+            GL.CompileShader(vertexShaderHandle);
+
+            int pixelShaderHandle = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(pixelShaderHandle, pixelShaderCode);
+            GL.CompileShader(pixelShaderHandle);
+
+            this.shaderProgramHandle = GL.CreateProgram();
+
+            GL.AttachShader(this.shaderProgramHandle, vertexShaderHandle);
+            GL.AttachShader(this.shaderProgramHandle, pixelShaderHandle);
+
+            GL.LinkProgram(this.shaderProgramHandle);
+
+            GL.DetachShader(this.shaderProgramHandle, vertexShaderHandle);
+            GL.DetachShader(this.shaderProgramHandle, pixelShaderHandle);
+
+            GL.DeleteShader(vertexShaderHandle);
+            GL.DeleteShader(pixelShaderHandle);
+
+            base.OnLoad();
+        }
+
+        protected override void OnUnload()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(this.vertexBufferHandle);
+
+            GL.UseProgram(0);
+            GL.DeleteProgram(this.shaderProgramHandle);
+
+            base.OnUnload();
+        }
+
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.UseProgram(this.shaderProgramHandle);
+            GL.BindVertexArray(this.vertexArrayHandle);
+
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+
+            // Dibuja las líneas para formar la "T"
+            GL.DrawArrays(PrimitiveType.LineLoop, 0, 4); // Parte superior de la "T"
+            GL.DrawArrays(PrimitiveType.LineLoop, 4, 4); // Parte vertical de la "T"
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
+
+
+            this.Context.SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+    }
+}
